9d790ad02b9c3db13229f1491c75a1450feed79b
Ankoromoti <ankoromoti.business@gmail.com>
2024-12-22 15:16:26 +0900

    new project

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Firebaseのプロジェクト作成時にFirebaseConfigが出てくるのでそれを貼り付ける
 const firebaseConfig = {
-    apiKey: "Your apiKey",
-    authDomain: "authDomain",
-    projectId: "storageBucket",
-    storageBucket: "storageBucket",
-    messagingSenderId: "messagingSenderId",
-    appId: "AppID"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
